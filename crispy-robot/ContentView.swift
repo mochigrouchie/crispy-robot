5a6055b376bf5bbbad5191a0e42ca5852a036785
Joshua Lim <joshua_lim@apple.com>
2022-03-26 21:41:31 +0800

    Initial Commit

---
+++ crispy-robot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  crispy-robot
+//
+//  Created by Joshua Lim on 26/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ crispy-robot/crispy_robotApp.swift
@@ -0,0 +1,17 @@
+//
+//  crispy_robotApp.swift
+//  crispy-robot
+//
+//  Created by Joshua Lim on 26/3/22.
+//
+
+import SwiftUI
+
+@main
+struct crispy_robotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
